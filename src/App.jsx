124abc07dffa76671f6c7adbd814c8ec6a562f4b
Ryan Sarcinelli <114365610+ryansarcinelli@users.noreply.github.com>
2025-07-25 15:02:54 -0300

    Add movie search UI and assets, update styles
    Introduces a new movie search interface with a Search component, updates App.jsx to display a hero banner and search input, and adds supporting image and SVG assets. CSS is updated for a background image and improved layout. Also adds placeholder MovieCard and Spinner components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Search from "./components/Search";
+import { useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [searchTerm, setSearchTerm] = useState('');
+   const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <div className="patter" />
 
-export default App
+      <div className="wrapper">
+        <header>
+          <img
+          src="./hero.png"
+          alt="Hero Banner"
+          className={`w-full max-w-lg h-auto object-contain mx-auto drop-shadow-md transform transition-all duration-7000 ease-out
+            ${show ? "opacity-100 translate-y-0 scale-100" : "opacity-0 translate-y-5 scale-90"}
+          `}
+        />
+          <h1>
+            Ache <span className="text-gradient">Filmes</span> sem dor de cabeça!
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+    </main>
+  );
+};
+
+export default App;
